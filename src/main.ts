59e9c48b310adeaa45616a0aac2a101120dde41f
extrange <29305375+extrange@users.noreply.github.com>
2023-05-17 08:39:23 +0000

    add sqlite persistence

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { Hocuspocus } from '@hocuspocus/server'
+import { Hocuspocus } from "@hocuspocus/server";
+import { SQLite } from "@hocuspocus/extension-sqlite";
 
-const server = new Hocuspocus({address: '0.0.0.0', port: 1234})
+const server = new Hocuspocus({
+  address: "0.0.0.0",
+  port: 1234,
+  extensions: [new SQLite({ database: "db.sqlite" })],
+});
 
-server.listen()
+server.listen();
