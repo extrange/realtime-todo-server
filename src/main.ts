a0c9ce6fe17fe90745d0c8df99b64d3f2ddb16dc
extrange <29305375+extrange@users.noreply.github.com>
2025-05-13 09:02:43 +0800

    chore(deps): update all

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { Hocuspocus } from "@hocuspocus/server";
+import { Server } from "@hocuspocus/server";
 import { SQLite } from "@hocuspocus/extension-sqlite";
 
-const server = new Hocuspocus({
-  address: "0.0.0.0",
-  port: 1234,
-  extensions: [new SQLite({ database: "db.sqlite" })],
+const server = new Server({
+	address: "0.0.0.0",
+	port: 1234,
+	extensions: [new SQLite({ database: "db.sqlite" })],
 });
 
 server.listen();
